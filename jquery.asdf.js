abd48bb7fcff8832c41bb8794bc5af39e464ed07
conradolega <conradolega@gmail.com>
2013-05-09 22:04:54 +0800

    Added minimum and maximum length and character set

--- jquery.asdf.js
+++ jquery.asdf.js
@@ -1,9 +1,19 @@
 (function ($) {
-	$.fn.asdf = function() {
-		this.each(function() {
-			if($(this).is('input:text')) {
-				$(this).val('asdf');
-			}
-		});
+	$.fn.asdf = function(min, max, char) {
+		if (min <= 0 && max >= min) {
+			var char = char || 'qwertasdfgzxcvb';
+			this.each(function() {
+				if($(this).is('input:text')) {
+					// String generation courtesy of:
+					// http://stackoverflow.com/a/14249522
+					var result = [];
+					var length = Math.floor((Math.random() * max) + min);
+					while (--length) {
+						result.push(char.charAt(Math.floor(Math.random() * char.length)));
+					}
+					$(this).val(result.join(''));
+				}
+			});
+		}
 	}	
 })(jQuery);
