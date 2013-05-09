1c351dd4d3b182598aea51f299a36ff582975280
conradolega <conradolega@gmail.com>
2013-05-10 05:08:08 +0800

    Testing

--- jquery.asdf.js
+++ jquery.asdf.js
@@ -1,16 +1,24 @@
 (function ($) {
-	$.fn.asdf = function(min, max, char) {
-		if (min <= 0 && max >= min) {
-			var char = char || 'qwertasdfgzxcvb';
+	$.fn.asdf = function(min, max, charset) {
+		console.log(min);
+		console.log(max);
+		console.log(charset);
+		if (min > 0 && max >= min) {
+			var charset = charset || 'asdf';
+			console.log(charset);
 			this.each(function() {
 				if($(this).is('input:text')) {
 					// String generation courtesy of:
 					// http://stackoverflow.com/a/14249522
 					var result = [];
-					var length = Math.floor((Math.random() * max) + min);
-					while (--length) {
-						result.push(char.charAt(Math.floor(Math.random() * char.length)));
+					var length = Math.floor((Math.random() * max)) + min;
+					console.log(length);
+					var i = 10;
+					while (--i && --length) {
+						console.log(length);
+						//result.push(charset.charAt(Math.floor(Math.random() * charset.length)));
 					}
+					console.log(result);
 					$(this).val(result.join(''));
 				}
 			});
