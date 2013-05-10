cad3faf1f887e69b688a706556cfbaaaa03661e2
conradolega <conradolega@gmail.com>
2013-05-10 20:13:14 +0800

    Fixed dynamic asdf generation

--- jquery.asdf.js
+++ jquery.asdf.js
@@ -1,24 +1,18 @@
 (function ($) {
 	$.fn.asdf = function(min, max, charset) {
-		console.log(min);
-		console.log(max);
-		console.log(charset);
+		var min = parseInt(min);
+		var max = parseInt(max);
 		if (min > 0 && max >= min) {
 			var charset = charset || 'asdf';
-			console.log(charset);
 			this.each(function() {
 				if($(this).is('input:text')) {
 					// String generation courtesy of:
 					// http://stackoverflow.com/a/14249522
 					var result = [];
-					var length = Math.floor((Math.random() * max)) + min;
-					console.log(length);
-					var i = 10;
-					while (--i && --length) {
-						console.log(length);
-						//result.push(charset.charAt(Math.floor(Math.random() * charset.length)));
+					var length = Math.floor(Math.random() * (max - min + 1)) + min;
+					while (--length) {
+						result.push(charset.charAt(Math.floor(Math.random() * charset.length)));
 					}
-					console.log(result);
 					$(this).val(result.join(''));
 				}
 			});
