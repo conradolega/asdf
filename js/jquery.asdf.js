1ab738fbeb80da2c00122a3ef57c551cc20a541d
conradolega <conradolega@gmail.com>
2013-05-10 20:22:31 +0800

    Updated asdf and added options to demo page

--- js/jquery.asdf.js
+++ js/jquery.asdf.js
@@ -1,9 +1,21 @@
 (function ($) {
-	$.fn.asdf = function() {
-		this.each(function() {
-			if($(this).is('input:text')) {
-				$(this).val('asdf');
-			}
-		});
+	$.fn.asdf = function(min, max, charset) {
+		var min = parseInt(min);
+		var max = parseInt(max);
+		if (min > 0 && max >= min) {
+			var charset = charset || 'asdf';
+			this.each(function() {
+				if($(this).is('input:text')) {
+					// String generation courtesy of:
+					// http://stackoverflow.com/a/14249522
+					var result = [];
+					var length = Math.floor(Math.random() * (max - min + 1)) + min;
+					while (--length) {
+						result.push(charset.charAt(Math.floor(Math.random() * charset.length)));
+					}
+					$(this).val(result.join(''));
+				}
+			});
+		}
 	}	
 })(jQuery);
